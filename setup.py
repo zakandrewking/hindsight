2d2bfb9c855bb97409047f218f8bb77a2ba956f1
Zak King <zaking17@gmail.com>
2018-07-24 14:55:19 -0700

    specify dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,16 @@ from setuptools import setup, Command, find_packages
 setup(
     name='hindsight',
     author='Zachary King',
+    python_requires='>=2.7,<3.0',
     packages=find_packages(),
+    install_requires=[
+        'cobra>=0.5,<0.6',
+        'jupyter',
+        'numpy',
+        'scipy',
+        'pandas',
+        'matplotlib_venn',
+        'seaborn',
+        'sklearn',
+    ],
 )
